f5183651b4c4ad952d1a3b5830dfbc5aec271748
ShvetkumarPatel <patelshvet@gmail.com>
2021-03-22 18:07:02 -0400

    Adjusting  Main Routing Configuration

--- place_booking_app/src/app/app-routing.module.ts
+++ place_booking_app/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'recipes', pathMatch: 'full' },
+  { path: '', redirectTo: 'places', pathMatch: 'full' },
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then( m => m.AuthPageModule)
-  },  {
+  },
+  {
     path: 'places',
     loadChildren: () => import('./places/places.module').then( m => m.PlacesPageModule)
   },
